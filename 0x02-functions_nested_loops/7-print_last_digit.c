65c79227c9824a98bfc0f7f3a597b56514e432ab
obinnamadu <favourobi4u@yahoo.com>
2022-10-18 18:42:45 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,14 @@
  */
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	int ld = (n % 10);
 
 	if (ld < 0)
-		ld *= -1;
+	{
+		ld = (-1 * ld);
+	}
 
 	_putchar(ld + '0');
 
-	return (0);
+	return (ld);
 }
