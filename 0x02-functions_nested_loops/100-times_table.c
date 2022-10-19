ec0168a9f8f57632f18cbd17464cd595994f636f
obinnamadu <favourobi4u@yahoo.com>
2022-10-18 19:31:38 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,8 @@ void print_times_table(int n)
 					_putchar((op % 10) + 48);
 				}
 				else
-				}															_putchar(((op / 100) % 10) + 48);
+				}
+					_putchar(((op / 100) % 10) + 48);
 					_putchar(((op / 10) % 10) + 48);
 					_putchar((op % 10) + 48);
 				}
