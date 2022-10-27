8c3e834373072ed3468454be29beef7d880eb408
obinnamadu <favourobi4u@yahoo.com>
2022-10-26 19:10:03 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *n)
 	while (str[i])
 	{
 		while (!(str[i] >= 'a' && str[i] <= 'z'))
-			i++
+			i++;
 		if (str[i - 1] == ' ' ||
 			str[i - 1] == '\t' ||
 			str[i - 1] == '\n' ||
