3c0bf009b681cfaf4a2fdabc3be4409be38e78cb
obinnamadu <favourobi4u@yahoo.com>
2022-11-30 14:22:38 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,22 @@
-include "lists.h"
-
+#include "lists.h"
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of elements
+ * print_listint - Entry Point
+ * @h: head
+ * Return: 0
  */
 size_t print_listint(const listint_t *h)
 {
-	register size_t count = 0;
+	int count = 0;
 
-	while (h)
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
+
 	return (count);
 }
